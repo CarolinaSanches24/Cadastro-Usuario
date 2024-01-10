f528f3e108646b362725b9262bc44c653d07ab06
CarolinaSanches24 <carolmosilva34@gmail.com>
2024-01-10 12:25:28 -0300

    feat:primeiro commit

---
+++ cadastrarUsuario.py
@@ -0,0 +1,10 @@
+print("Cadastrar Usuário");
+
+username = input("Digite seu nome:");
+mail = input("Digite seu email");
+
+def cadastrar_usuario():
+    print("Usuario cadastrado com Sucesso!");
+
+cadastrarUsuario();
+   
